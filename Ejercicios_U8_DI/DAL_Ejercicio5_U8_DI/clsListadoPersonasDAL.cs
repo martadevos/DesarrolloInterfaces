1fe9925d001a705b58d708d920f8c70a4aa8866b
mdevos <marta.devos@iesnervion.es>
2022-11-21 21:30:54 +0100

    ACABAR Ejercicio5 U8

---
+++ Ejercicios_U8_DI/DAL_Ejercicio5_U8_DI/clsListadoPersonasDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades_Ejercicio5_U8_DI;
+
+namespace DAL_Ejercicio5_U8_DI
+{
+    public class clsListadoPersonasDAL
+    {
+        /// <summary>
+    	/// Función que nos devuelve un listado de todas las personas
+    	/// </summary>
+    	/// <returns>Listado de personas</returns>
+    	public List<clsPersona> getListadoCompletoPersonas()
+        {
+            List<clsPersona> personas = new List<clsPersona>();
+            personas.Add(new clsPersona("Marta", "Devós", "09/12/2003"));
+            personas.Add(new clsPersona("Juan José", "Muñoz", "06/07/2000"));
+            personas.Add(new clsPersona("Rafael David", "Devos", "06/01/1974"));
+            personas.Add(new clsPersona("Marta", "Devos", "21/10/1972"));
+            personas.Add(new clsPersona("Marta", "Garcia", "09/12/2003"));
+            personas.Add(new clsPersona("Berta", "Devos", "06/01/2005"));
+            personas.Add(new clsPersona("Fernando", "Devos", "07/05/2099"));
+            personas.Add(new clsPersona("Agustin", "Devos", "07/07/2003"));
+        }
+
+    }
+}
---
+++ Ejercicios_U8_DI/Entidades_Ejercicio5_U8_DI/clsPersona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_Ejercicio5_U8_DI
+{
+    public class clsPersona
+    {
+        #region Atributos
+        private string nombre;
+        private string apellidos;
+        private string fechaNac;
+        #endregion
+
+        #region Propiedades
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Apellidos { get { return apellidos; } set { apellidos = value; } }
+        public string FechaNac { get { return fechaNac; } set { fechaNac = value; } }
+        #endregion
+
+        #region Constructores
+        public clsPersona(string nombre, string apellidos, string fechaNac)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fechaNac = fechaNac;
+        }
+
+        public clsPersona()
+        {
+        }
+        #endregion
+    }
+}
